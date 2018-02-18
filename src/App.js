db0c4774fee7e135dc0dcbd3329bf4a3b9f6da15
alvaroenrique <alvaroenriquehl@gmail.com>
2018-02-18 13:30:28 -0500

    Commit inicial con el router de ejemplo

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import BasicExample from './components/BasicExample';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <BasicExample />
+      </div>
+    );
+  }
+}
+
+export default App;
